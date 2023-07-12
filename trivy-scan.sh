13a08d6e36b48e79e6db3f6fc3e9e4e2ad09cb69
hhk14 <harishahk14@gmail.com>
2023-07-12 15:54:14 +0530

    added pre docker build image scan stage

--- trivy-scan.sh
+++ trivy-scan.sh
@@ -3,7 +3,7 @@ dockerImage=`awk -F " " 'NR==1 {print $NF}' Dockerfile`
 echo $dockerImage
 trivy image -q --exit-code 1 --severity CRITICAL $dockerImage
 result="$?"
-if [ result -eq 0 ]
+if [ $result -eq 0 ]
 then
     echo "Image Scan Passed. No vulnerabilities found!"
     exit 1
